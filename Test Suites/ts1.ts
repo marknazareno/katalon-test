3ca297e79be6853728112dea21226ea1e17a1909
marknazareno <marknazareno@marks-mbp.lan>
2019-02-09 14:43:09 +0800

    test

---
+++ Test Suites/ts1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>654455e4-8bd9-406b-bb16-bf764d4d999c</testSuiteGuid>
+   <testCaseLink>
+      <guid>53aaba3f-6972-4473-b6fd-0043bd491319</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test1</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>44c5b52c-5c5b-4416-abce-4a5e2a9aec08</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
